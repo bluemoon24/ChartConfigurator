42da92f9e6e5e466725ae052538df0b8436bebac
Lothar Burow <lothar.burow@bayer.com>
2018-09-20 00:09:31 +0200

    Initial commit

---
+++ src/components/ConfigControls.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card   >
+      <v-card-title>Hello</v-card-title>
+      <v-card-text>
+      <v-text-field label="No data error message " :value="config.noDataMessage()"></v-text-field>
+      <v-text-field label="Categories Title " :value="config.categoriesTitle"></v-text-field>
+
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'config-controls',
+    props: {
+      config: {
+        type: Object,
+        default: {}
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
